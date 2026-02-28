ccd984d468858f5bee093e20473c39b2c46c6892
yu_liangbin <y1416490440@163.com>
2026-02-28 15:59:37 +0800

    [msprof-master]PART_4:新增msprof7个冒烟用例
    Co-authored-by: yuliangbin<y1416490440@163.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !94 merge msprof_st_part4 into master
    
    [msprof-master]PART_4:新增msprof7个冒烟用例
    
    Created-by: yu_liangbin
    Commit-by: yuliangbin
    Merged-by: ascend-robot
    Description: # PR 合入模板
    
    **注：经过自检不涉及的可标注“不涉及”或直接打勾，特殊情况请文字备注。不符合规范的 PR 不允许合入，请（后备）commit 注意。**
    
    ----
    
    ## 1. 修改描述
    - **修改原因：**  
    需要补充msprof的冒烟用例
    - **修改内容：**  
    1. 补充参数task_based的5个msprof冒烟用例
    2. 补充参数sample_based+memory_ub的msprof冒烟用例
    3. 补充参数sample_based+resource_conflict_ratio的msprof冒烟用例
    
    ----
    
    ## 2. 功能验证
    - [ ] **功能自验**
    - [ ] **本地自验用例截图**
    - [ ] **冒烟是否通过** （填入群链接的自验证报告中，如未通过，请说明原因：____________________ ，功能代码请主动申报添加冒烟）
    
    ----
    
    ## 3. 分支合并要求
    - [ ] **代码合并**（请确保将 master 分支的最新代码同步合并至 poc 分支及 pre-research 分支，同时保证 poc 分支的代码也已正确合并到 pre-research 分支。）
    
    ----
    
    ## 3. 代码检视
    - **要求：**
      - 合入代码超过 200 行，需三人以上会议检视。
      - 检视密度≥1个/100行。
      - 检视缺陷密度未达要求需提供说明。
      - 大于 1000 行代码原则上不允许合入，需进行备案。
    - [ ] **是否经过代码检视**
    - [ ] **是否具备 UT 测试用例看护** （如不符合，请说明原因：____________________）
    
    - **检视意见数：____ 条** （请填写本次检视的意见总数，用于commit合入前审视）
    
    ----
    
    ## 4. 安全自检
    
    ### Python、C++：
    - [ ] **对外接口新增/删除/变更后，资料要同步新增/删除/变更，新增接口入参校验参考外部输入表格**
    - [ ] **不允许私有的文件操作，需要使用公共模块的安全函数**
    - [ ] **任务结束后需要删除临时文件，同时需要考虑任务失败后，临时文件没有残留**
    - [ ] **数组访问需要校验越界场景，对除法需要做除零校验**
    - [ ] **需要对递归方法做递归深度校验，正则表达式必须做 ReDoS 校验**
    - [ ] **需要充分进行接口输入和返回值异常情况的校验**
    - [ ] **日志打印不要出现拼写或语法错误，不要暴露代码细节和敏感信息**
    
    ### C++：
    - [ ] **指针使用前需要判空**
    - [ ] **数值计算校验溢出和反转**
    - [ ] **不可存在内存泄漏（异常场景需要释放内存）**
    - [ ] **类型转换不能出现数据截断**
    - [ ] **拷贝字符串时，目的缓冲区至少比源缓冲区大 1**
    - [ ] **拷贝内存时，目的缓冲区不小于源缓冲区**
    - [ ] **内存释放后指针赋值为 nullptr**
    
    
    ----
    
    ## 5. 变更知会
    - [ ] **资料修改**
    - [ ] **变更通知（消息知会 + 邮件知会）**
    
    ----
    
    See merge request: Ascend/msprof!94

---
+++ test/st/l1_test_ascend_msprof_app_task_based_memory.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+file_name=$(basename $0 .sh)
+
+cur_dir="$(cd "$(dirname "$0")"; pwd)"
+
+ouput_path=$1
+
+MSPROF_OPTS="--ascendcl=on --model-execution=on --runtime-api=on --task-time=on --ai-core=on --l2=on --aic-freq=100 --aicpu=on --sys-hardware-mem=on --aic-mode=task-based --llc-profiling=write --aic-metrics=Memory"
+MODEL_NAME="resnet50"
+
+chmod 755 ${cur_dir}/base_msprof.sh
+"$cur_dir/base_msprof.sh" "$file_name" "${MSPROF_OPTS}" "${MODEL_NAME}" "${ouput_path}"
---
+++ test/st/src/l1_test_ascend_msprof_app_task_based_memory_l0.py
@@ -0,0 +1,83 @@
+# -------------------------------------------------------------------------
+# Copyright (c) 2026 Huawei Technologies Co., Ltd.
+# This file is part of the MindStudio project.
+#
+# MindStudio is licensed under Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#
+#    http://license.coscl.org.cn/MulanPSL2
+#
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more details.
+# -------------------------------------------------------------------------
+import glob
+import sys
+import logging
+
+from utils.file_check import FileChecker
+from src.base_ascend_msprof_checker import BaseAscendMsprofChecker
+
+logging.basicConfig(level=logging.INFO,
+                    format='\n%(asctime)s %(filename)s [line:%(lineno)d] [%(levelname)s] %(message)s')
+
+
+class TestAscendMsprofAppTaskBasedMemoryL0(BaseAscendMsprofChecker):
+    def check_ai_vector_core_utilization_data(self):
+        super().check_ai_vector_core_utilization_data()
+
+    def check_time_line_data(self):
+        super().check_time_line_data()
+
+    def check_op_summary_data(self):
+        super().check_op_summary_data()
+
+    def check_op_statistic_data(self):
+        super().check_op_statistic_data()
+
+    def check_ai_core_utilization_data(self):
+        super().check_ai_core_utilization_data()
+
+    def check_npu_mem_data(self):
+        super().check_npu_mem_data()
+
+    def check_npu_module_mem_data(self):
+        super().check_npu_module_mem_data()
+
+    def check_soc_pmu_data(self):
+        super().check_soc_pmu_data()
+
+    def check_task_time_data(self):
+        super().check_task_time_data()
+
+    def check_api_statistic_data(self):
+        super().check_api_statistic_data()
+
+    def check_hbm_data(self):
+        super().check_hbm_data()
+
+    def check_l2_cache_data(self):
+        super().check_l2_cache_data()
+
+    def check_llc_read_write_data(self):
+        super().check_llc_read_write_data()
+
+    def check_other_data(self):
+        super().check_other_data()
+
+
+def test_ascend_msprof_app_task_based_memory_l0(prof_path):
+    expect_db_tables = ["ACC_PMU", "AICORE_FREQ", "CANN_API", "COMPUTE_TASK_INFO", "ENUM_API_TYPE",
+                        "ENUM_HCCL_DATA_TYPE", "ENUM_HCCL_LINK_TYPE", "ENUM_HCCL_RDMA_TYPE", "ENUM_HCCL_TRANSPORT_TYPE",
+                        "ENUM_MEMCPY_OPERATION", "ENUM_MODULE", "ENUM_MSTX_EVENT_TYPE", "HBM", "HOST_INFO", "LLC",
+                        "MEMCPY_INFO", "META_DATA", "NPU_INFO", "NPU_MEM", "NPU_MODULE_MEM", "QOS", "RANK_DEVICE_MAP",
+                        "SESSION_TIME_INFO", "SOC_BANDWIDTH_LEVEL", "STRING_IDS", "TASK", "TASK_PMU_INFO"]
+    test_ascend_msprof = TestAscendMsprofAppTaskBasedMemoryL0(prof_path, expect_db_tables)
+    test_ascend_msprof.check_msprof_file()
+
+
+if __name__ == '__main__':
+    path = sys.argv[1]
+    test_ascend_msprof_app_task_based_memory_l0(path)
